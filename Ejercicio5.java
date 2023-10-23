51365866c9628c4ba5217802403e81ae830243aa
Diego261099 <115853971+Diego261099@users.noreply.github.com>
2023-10-22 19:21:27 -0500

    Create Ejercicio5.java

---
+++ Ejercicio5.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ReverseWordTest {
+
+    //probamos con una sola palabra
+	@Test
+    public void testReverseWordPalabraSimple() {
+        String input = "Hello";
+        String expected = "olleH ";
+        String result = ReverseWord.reverseWord(input);
+        assertEquals(expected, result);
+    }
+
+    //aqui probamos con una oraciones que contiene más de 1 palabra
+    @Test
+    public void testReverseWordOracion() {
+        String input = "This is a test";
+        String expected = "sihT si a tset ";
+        String result = ReverseWord.reverseWord(input);
+        assertEquals(expected, result);
+    }
+
+    //se prueba con una cadena vacia
+    @Test
+    public void testReverseWordCadenaVacia() {
+        String input = "";
+        String expected = "";
+        String result = ReverseWord.reverseWord(input);
+        assertEquals(expected, result);
+    }
+
+    //probamos con caracteres especiales
+    @Test
+    public void testReverseWordCaracteresEspeciales() {
+        String input = "123!@#$";
+        String expected = "321!@#$ ";
+        String result = ReverseWord.reverseWord(input);
+        assertEquals(expected, result);
+    }
+}
+Footer
+© 2023 GitHub, Inc.
